89a6b99def3b3075646aa88f1e5332c98fa258ca
Ruafel <ruafelianna@ya.ru>
2024-09-25 16:10:31 +0300

    Add auto-init and reset defaults; add IsChanged implementation

--- Andromeda.Components.Forms/SaveableForm.cs
+++ Andromeda.Components.Forms/SaveableForm.cs
@@ -1,10 +1,12 @@
 using Andromeda.Components.Forms.Abstractions;
+using DynamicData.Binding;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Andromeda.Components.Forms
@@ -20,10 +22,42 @@ namespace Andromeda.Components.Forms
             IObservable<bool>? canSaveExt = null
         ) : base(initState)
         {
+            _selfType = This.GetType();
+
+            _propertiesWithDefaults = FormFields
+                .Select(x => new PropertyInfo?[] {
+                    _selfType.GetProperty(x.PropertyName),
+                    _selfType.GetProperty(
+                        $"Default_{x.PropertyName}",
+                        BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                    )
+                })
+                .Where(x => x[1] is not null);
+
+            IsChanged = this
+                .WhenAnyPropertyChanged(_propertiesWithDefaults
+                    .SelectMany(x => x)
+                    .Select(x => x!.Name)
+                    .ToArray()
+                )
+                .Select(vm => {
+                    foreach (var prop in _propertiesWithDefaults)
+                    {
+                        var (val, def) = GetProps(prop!);
+
+                        if (val != def)
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                });
+
             var canSaveSources = new List<IObservable<bool>>(3)
             {
                 ValidationContext.Valid,
-                IsChanged.Select(x => !x)
+                IsChanged
             };
 
             if (canSaveExt is not null)
@@ -43,10 +77,46 @@ namespace Andromeda.Components.Forms
 
         public IObservable<bool> CanSave { get; }
 
-        public abstract IObservable<bool> IsChanged { get; }
+        public IObservable<bool> IsChanged { get; }
 
         public IReactiveCommand Save { get; }
 
         protected TSelf This => (TSelf)this;
+
+        private readonly IEnumerable<PropertyInfo?[]> _propertiesWithDefaults;
+
+        private readonly Type _selfType;
+
+        protected void InitDefaultsAuto()
+        {
+            foreach (var prop in _propertiesWithDefaults)
+            {
+                var type = prop[0]!.PropertyType;
+                prop[1]!.SetValue(This, type.IsValueType
+                    ? Activator.CreateInstance(type)
+                    : null
+                );
+            }
+        }
+
+        protected void SetDefaultsAuto()
+        {
+            foreach (var prop in _propertiesWithDefaults)
+            {
+                var (_, def) = GetProps(prop!);
+
+                prop[0]!.SetValue(This, def);
+            }
+        }
+
+        private (object? val, object? def) GetProps(
+            PropertyInfo[] pi
+        )
+        {
+            object? GetPropVal(int index)
+                => pi[index]!.GetValue(This);
+
+            return (GetPropVal(0), GetPropVal(1));
+        }
     }
 }
